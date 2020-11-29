2b1820e489c3563057ded1d9beee0dbe572c8b86
KAEVO <106756044+KAEVO@users.noreply.github.com>
2020-11-29 14:40:25 +0000

    Final report

--- builder.go
+++ builder.go
@@ -5,3 +5,11 @@ import (
 	"encoding/csv"
 	"io/ioutil"
 	"net/http"
+)
+
+// Builder is main type for this tool.
+type Builder struct {
+	BaseURL    string
+	featureMap map[string]*Feature
+	data       [][]string // Strings of Data to be read in to CSV
+	records    int        // Number of records to be retrieved for dataset
