e487bdf7bbb6ac105f3063594489e20911fc3c1d
KAEVO <106756044+KAEVO@users.noreply.github.com>
2020-12-26 19:30:28 +0000

    Rebuild packages

--- builder_test.go
+++ builder_test.go
@@ -2,3 +2,17 @@
 package dsbldr
 
 import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"reflect"
+	"testing"
+)
+
+func TestNewBuilder(t *testing.T) {
+	b := NewBuilder(4, 100)
+	if got, want := len(b.data), 101; got != want {
+		t.Fatalf("got: %v\n want: %v\n ", got, want)
+	}
+	if got, want := len(b.data[0]), 4; got != want {
