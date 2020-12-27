441f0c8fff5df62c6edaad6f69186073d9662774
KAEVO <106756044+KAEVO@users.noreply.github.com>
2020-12-27 06:26:13 +0000

    remove unused code.

--- builder_test.go
+++ builder_test.go
@@ -16,3 +16,17 @@ func TestNewBuilder(t *testing.T) {
 		t.Fatalf("got: %v\n want: %v\n ", got, want)
 	}
 	if got, want := len(b.data[0]), 4; got != want {
+		t.Fatalf("got: %v\n want: %v\n ", got, want)
+	}
+	if got, want := b.records, 100; got != want {
+		t.Fatalf("got: %v\n want: %v\n ", got, want)
+	}
+}
+
+func TestAddFeatureData(t *testing.T) {
+	b := NewBuilder(4, 3)
+	b.addFeatureData("newFeature", []string{"one", "two", "three"})
+	expectedData := [][]string{
+		[]string{"newFeature", "", "", ""},
+		[]string{"one", "", "", ""},
+		[]string{"two", "", "", ""},
