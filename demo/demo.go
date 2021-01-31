49727dea57b9ed9ab820db2a1779b37d12363fca
KAEVO <106756044+KAEVO@users.noreply.github.com>
2021-01-31 11:58:42 +0000

    intelliactive: add upstream patches

--- demo/demo.go
+++ demo/demo.go
@@ -9,4 +9,5 @@ import (
 builder d.Builder := d.Builder{
 	BaseURL: "localhost:8080",
 	RequestHeaders: map[string]string{
-		"Authorization": BasicOAuthHeader
+		"Authorization": BasicOAuthHeader(
+			"OAUTH_C
