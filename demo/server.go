56e9dd2d662709ebb0311c507906ef05532700d4
KAEVO <106756044+KAEVO@users.noreply.github.com>
2021-05-01 13:23:18 +0000

    Added map

--- demo/server.go
+++ demo/server.go
@@ -1,2 +1,17 @@
 
 package main
+
+/*
+Demo server that spits out 1000 random records of a certain kind in a response
+*/
+
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+const (
+	recordCount    = 1000
