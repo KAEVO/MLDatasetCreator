f4989a490633dd4fb835d235de63afc7d5ad134f
KAEVO <106756044+KAEVO@users.noreply.github.com>
2021-05-23 06:54:11 +0000

    Fix conflicts.

--- features.go
+++ features.go
@@ -7,4 +7,4 @@ import (
 
 // RunFunc holds the computation that processes the API responses to features
 // is sent an array of JSON strings as the responses ??as well as a map of data from the features parent features??
-// Basically
+// Basically what you do
