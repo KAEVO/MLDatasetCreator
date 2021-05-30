2d9b37ecaa9198984fa2af89a93914e72a5cc104
KAEVO <106756044+KAEVO@users.noreply.github.com>
2021-05-30 19:39:36 +0000

    Fix help text

--- features.go
+++ features.go
@@ -8,4 +8,4 @@ import (
 // RunFunc holds the computation that processes the API responses to features
 // is sent an array of JSON strings as the responses ??as well as a map of data from the features parent features??
 // Basically what you do with the run function is take in a string of
-// serialized API data (could be in JSON 
+// serialized API data (could be in JSON or XML), do pars
