1d1cbcd1e0f17abe74ec0d529e14d8e81ee883d4
KAEVO <106756044+KAEVO@users.noreply.github.com>
2021-06-27 09:44:45 +0000

    Update test runner

--- features.go
+++ features.go
@@ -15,3 +15,4 @@ type RunFunc func(responses []string) []string // parents map[string]string
 
 // Structs representing RetreiveType
 // SingleRetrieve Features only require one request to create the JSON Dump
+// that's pas
