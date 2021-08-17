4a36c08a2d8e8e83d683acf65bcb42d2ab4dabc3
KAEVO <106756044+KAEVO@users.noreply.github.com>
2021-08-17 13:23:25 +0000

    Auto-format

--- features.go
+++ features.go
@@ -31,4 +31,4 @@ type Feature struct {
 	Name         string
 	Endpoint     string  // API Endpoint
 	RunFunc      RunFunc // function that performs ad-hoc computation
-	RetrieveType int     /
+	RetrieveType int     // Determin
