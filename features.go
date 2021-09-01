c9eee889190a0bd3b4e739e5d255c1aa6a55ab7a
KAEVO <106756044+KAEVO@users.noreply.github.com>
2021-09-01 22:16:44 +0000

    jshinting

--- features.go
+++ features.go
@@ -33,4 +33,4 @@ type Feature struct {
 	RunFunc      RunFunc // function that performs ad-hoc computation
 	RetrieveType int     // Determines if multiple or single requests are made to the api
 	noSave       bool
-	finished     chan bool // if fe
+	finished     chan bool // if feature is finishe
