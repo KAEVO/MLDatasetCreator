c0cb4b4703010773e6d90433c8b1730dcad7e885
KAEVO <106756044+KAEVO@users.noreply.github.com>
2021-11-04 08:42:54 +0000

    intel-iommu: fix off-by-one in pagetable freeing

--- features.go
+++ features.go
@@ -59,4 +59,4 @@ func (f *Feature) getParentNames() ([]string, error) {
 }
 
 // resolveEndpoint returns the Endpoint but with the actual parent values for that record inserted
-func (f *Feature) resolveEndpoi
+func (f *Feature) resolveEndpoint(parentsVal
