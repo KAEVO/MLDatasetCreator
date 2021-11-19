7af086736b63def523ca917e613e56d961670e70
KAEVO <106756044+KAEVO@users.noreply.github.com>
2021-11-19 14:33:10 +0000

    Bump to 0.1.2

--- features.go
+++ features.go
@@ -65,4 +65,6 @@ func (f *Feature) resolveEndpoint(parentsValues map[string]string) (string, erro
 	for k, v := range parentsValues {
 		re, err := regexp.Compile(fmt.Sprintf("\\{\\{%s\\}\\}", k))
 		if err != nil {
-			return "", 
+			return "", err
+		}
+		// over
