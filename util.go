db2d4a918d2427080af938157bcc501be5a05545
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-01-05 12:53:48 +0000

    Update version to 2.0.1

--- util.go
+++ util.go
@@ -5,4 +5,4 @@ import (
 )
 
 // BasicOAuthHeader spits out a basic OAuth Header based on access token
-func BasicOAuthHeader(consumerKey, nonce, signature, 
+func BasicOAuthHeader(consumerKey, nonce, signature, signatureMethod,
