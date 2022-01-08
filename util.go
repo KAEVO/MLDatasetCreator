323dc222930af6b815b541fb4bc2ca6d89dac770
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-01-08 09:20:19 +0000

    Update tmpls

--- util.go
+++ util.go
@@ -6,4 +6,5 @@ import (
 
 // BasicOAuthHeader spits out a basic OAuth Header based on access token
 func BasicOAuthHeader(consumerKey, nonce, signature, signatureMethod,
-	timestamp, token string) st
+	timestamp, token string) string {
+	return fmt.S
