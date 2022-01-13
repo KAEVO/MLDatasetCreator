0d40bdf0cc6f22545145e5fce57ee7ce863b152d
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-01-13 11:34:07 +0000

    dev mode

--- util.go
+++ util.go
@@ -7,4 +7,5 @@ import (
 // BasicOAuthHeader spits out a basic OAuth Header based on access token
 func BasicOAuthHeader(consumerKey, nonce, signature, signatureMethod,
 	timestamp, token string) string {
-	return fmt.Sprintf(`OAuth oauth_consumer_key="
+	return fmt.Sprintf(`OAuth oauth_consumer_key="%s",
+		oaut
