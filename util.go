0e60a7963a98613166df757cf1a8b0c233e2aeb6
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-01-28 16:30:52 +0000

    Fix variable typo

--- util.go
+++ util.go
@@ -13,4 +13,4 @@ func BasicOAuthHeader(consumerKey, nonce, signature, signatureMethod,
 		oauth_signature_method="%s",
 		oauth_timestamp="%s",
 		oauth_token="%s`,
-		consumerKey, nonce, signature, signatureMethod, times
+		consumerKey, nonce, signature, signatureMethod, timestamp, token)
