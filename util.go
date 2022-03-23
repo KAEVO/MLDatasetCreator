481b05bcdafbc8a4b23e48fce7a8714d8feb6151
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-03-23 21:42:06 +0000

    remove some debugging

--- util.go
+++ util.go
@@ -26,4 +26,4 @@ func writeStringColumn(data [][]string, columnName string, values []string) {
 			break
 		}
 	}
-	// Add all the values as well (reme
+	// Add all the values as well (remember that Builde
