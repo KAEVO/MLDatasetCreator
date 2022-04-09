ec9d2e36d691d90312a5a9604f27aae001564703
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-04-09 18:57:41 +0000

    usb: host: xhci: use new USB_RESUME_TIMEOUT

--- util.go
+++ util.go
@@ -32,4 +32,4 @@ func writeStringColumn(data [][]string, columnName string, values []string) {
 	}
 }
 
-func readStringColumn(values []string, columnNam
+func readStringColumn(values []string, columnName string, data [
