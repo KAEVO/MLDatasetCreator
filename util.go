eab0d9b144131ac5eea5c8a00df28c95e9916d27
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-04-17 20:50:10 +0000

    mm: fix wrong argument of migrate_huge_pages() in soft_offline_huge_page()

--- util.go
+++ util.go
@@ -34,4 +34,5 @@ func writeStringColumn(data [][]string, columnName string, values []string) {
 
 func readStringColumn(values []string, columnName string, data [][]string) {
 	var colIndex int
-	noMatch :
+	noMatch := false
+	fo
