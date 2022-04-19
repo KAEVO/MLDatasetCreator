7e9e6d95db8ad892b51c621a02aa98c9ee7e5914
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-04-19 10:49:23 +0000

    Typos in comments

--- util.go
+++ util.go
@@ -35,4 +35,5 @@ func writeStringColumn(data [][]string, columnName string, values []string) {
 func readStringColumn(values []string, columnName string, data [][]string) {
 	var colIndex int
 	noMatch := false
-	for i := rang
+	for i := range data[0] {
+	
