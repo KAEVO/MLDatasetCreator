bfbf2899868c9ddc13c60afd37ba269ecd066ad6
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-05-07 11:18:11 +0000

    fix gcc build

--- util.go
+++ util.go
@@ -45,4 +45,5 @@ func readStringColumn(values []string, columnName string, data [][]string) {
 		}
 	}
 	// Add all the values as well (remember that Builder.data is pre-allocated)
-	for i := 1; i < len(da
+	for i := 1; i < len(data); i++ {
+		if n
