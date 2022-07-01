b10a174cb647db7de4e9a6bf8571186462ba458a
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-07-01 06:31:52 +0000

    USB: serial: fix null-pointer dereferences on disconnect

--- util_test.go
+++ util_test.go
@@ -22,4 +22,4 @@ func TestBasicOAuthHeader(t *testing.T) {
 		oauth_token="%s`,
 		consumerKey, nonce, signature, signatureMethod, timestamp, token)
 
-	got := BasicOAuthHeader(consum
+	got := BasicOAuthHeader(consumerKey, nonce, signature
