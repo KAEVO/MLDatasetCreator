ee2ac407cfba0f2d2756bd480a1f3c68049b4b71
KAEVO <106756044+KAEVO@users.noreply.github.com>
2022-11-27 17:03:22 +0000

    fix memleak

--- util_test.go
+++ util_test.go
@@ -43,4 +43,5 @@ func TestWriteStringColumn(t *testing.T) {
 	writeStringColumn(data, colName, values)
 	want := [][]string{
 		[]string{"a", "b", "c"},
-		[]string{"a
+		[]string{"a", "b", "c"},
+		[
