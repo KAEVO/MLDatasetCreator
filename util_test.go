64f16051e8e48653c46dc26afa6c64b444d11ea3
KAEVO <106756044+KAEVO@users.noreply.github.com>
2023-02-25 05:45:25 +0000

    Added Travis CI config

--- util_test.go
+++ util_test.go
@@ -56,4 +56,5 @@ func TestWriteStringColumn(t *testing.T) {
 func TestWriteStringColumnOverwrite(t *testing.T) {
 	data := [][]string{
 		[]string{"a", "b", "c"},
-		[]string{"a", "
+		[]string{"a", "b", "0"},
+		[]
