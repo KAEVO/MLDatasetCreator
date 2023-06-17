f8f2c6d16b6a6a1fff02e5ae87ecf97a871a91d8
KAEVO <106756044+KAEVO@users.noreply.github.com>
2023-06-17 16:30:25 +0000

    ax5select

--- util_test.go
+++ util_test.go
@@ -105,4 +105,4 @@ func TestReadStringColumnWithNoHeader(t *testing.T) {
 	values := make([]string, 3)
 
 	want := []string{"", "", ""}
-	readStringColumn(values, 
+	readStringColumn(values, colName, 
